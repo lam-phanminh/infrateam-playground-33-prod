979bcffd28d32f934ac9a9290b2313c5c3da18d8
lam-phanminh <85839102+lam-phanminh@users.noreply.github.com>
2025-07-04 14:07:55 +0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,3 +4,10 @@ terraform {
     prefix = "infrateam-tf/state"
   }
 }
+
+resource "google_storage_bucket" "infra_test" {
+  name                        = "infra-test-12345"
+  project                     = var.project_id
+  location                    = var.region
+  uniform_bucket_level_access = true
+}
